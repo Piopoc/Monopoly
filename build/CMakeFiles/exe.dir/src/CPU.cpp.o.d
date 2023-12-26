ab6a3db312c72b9e33864a5597390421dd5a48d3
Filippo Corradi <filippo.corradi@studenti.unipd.it>
2023-12-26 16:02:25 +0100

    cartella build 2.0

---
+++ build/CMakeFiles/exe.dir/src/CPU.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/exe.dir/src/CPU.cpp.o: \
+ /home/piopo/Documenti/C++/monopoli/src/CPU.cpp \
+ /usr/include/stdc-predef.h
---
+++ build/CMakeFiles/exe.dir/src/humanPlayer.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/exe.dir/src/humanPlayer.cpp.o: \
+ /home/piopo/Documenti/C++/monopoli/src/humanPlayer.cpp \
+ /usr/include/stdc-predef.h
