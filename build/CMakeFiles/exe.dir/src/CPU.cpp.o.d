f1682ebaa3e5fa47003203ff9e28fb10f4480160
Filippo Corradi <filippo.corradi@studenti.unipd.it>
2023-12-26 12:04:45 +0100

    gitignore

--- build/CMakeFiles/exe.dir/src/CPU.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/exe.dir/src/CPU.cpp.o: \
- /home/piopo/Documenti/C++/monopoli/src/CPU.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/exe.dir/src/humanPlayer.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/exe.dir/src/humanPlayer.cpp.o: \
- /home/piopo/Documenti/C++/monopoli/src/humanPlayer.cpp \
- /usr/include/stdc-predef.h
