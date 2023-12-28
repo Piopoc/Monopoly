5396e9efcd246edcb39e2180c30b82a6aa95b297
levalby <levalby@gmail.com>
2023-12-28 12:34:14 +0100

    sistemo bankAccount

--- include/player.h
+++ include/player.h
@@ -1,4 +1,4 @@
-/* #ifndef PLAYER_H
+#ifndef PLAYER_H
 #define PLAYER_H
 
 #include <iostream>
@@ -6,10 +6,15 @@
 class player{
     public:
         virtual player(std::string numeroPlayer)=0; // costruttore che deve essere sovrascritto sia da CPU che humanPlayer --> virtuale da overloading
-        void move(); // funzione di spostamento nel tabellone
+        void move(int passi); // funzione di spostamento nel tabellone
+        bool hasMoney();
+        int saldo();
+        void versa(int x);
+        void preleva(int x);
     private:
         int cash;
         bool belongTo; // ??
+
 };
 
-#endif */
+#endif
--- src/player.cpp
+++ src/player.cpp
@@ -1,5 +1,20 @@
-/* #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <iostream>
 #include "../include/player.h"
- */
+
+bool player::hasMoney()
+{
+    return cash == 0;
+}
+int player::saldo()
+{
+    return cash;
+}
+void player::versa(int x)
+{
+    cash += x;
+}
+void player::preleva(int x)
+{
+    if(deposito-x<0) throw std::exception();
+    cash -= x;
+}
