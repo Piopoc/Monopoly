723608857f0c4a1e3a6cf71cb3251383f35f0ff8
levalby <levalby@gmail.com>
2023-12-28 19:16:53 +0100

    commenti alla funzioni

--- include/player.h
+++ include/player.h
@@ -5,18 +5,25 @@
 
 class Player{
     public:
+    //costruttore con id player
         Player(int ID);
+    //controllo se giocatore ha denaro
         bool hasMoney();
+    //ottiene il saldo giocatore
         int saldo();
+    //versa una somma di denaro al giocatore
         void versa(int x);
+    //preleva una somma di denaro al giocatore, se non c'è disponibilità lancia eccezione, si presuppone che nel main venga controllato prima se c'è disponibilità per non gestire in try catch
         void preleva(int x);
-        void setID(int x);
+    //ottieni id del giocatore
         int getID();
     private:
+    //fiorini
         int cash;
-        bool belongTo; // ??
+    //id del giocatore
         int playerID;
 
 };
+//lancia 2 dadi e ottieni somma
 int dadi();
 #endif
