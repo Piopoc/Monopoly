cceb629310802e0ce38779e11e303452260809d9
levalby <levalby@gmail.com>
2023-12-30 17:16:42 +0100

    scrittura file computer.log

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <queue>
-#include <ifstream>
+#include <fstream>
 #include "../include/tabellone.h"
 #include "../include/Player.h"
 #include "../include/Cell.h"
@@ -34,44 +34,46 @@ int main(int argc, char* argv[]){
     pList.push(p4);
     
     if (modalitaGioco == "computer") {
-        std::ifstream fs("computer.log");
-        if(!fs.good()) throw std::exception();
+        std::ofstream ofs("computer.log",ofstream::out);
+        if(!ofs.good()) throw std::exception();
 
         while(pList.size()!=1){
             Player& pt = pList.front(); //player del turno
             pList.pop();
             int lancio = dadi();
             int playerID = pt.getID();
-            cout<<"Giocatore "<<playerID<<" ha tirato i dadi ottenendo un valore di "<<lancio<<endl;
+            ofs<<"Giocatore "<<playerID<<" ha tirato i dadi ottenendo un valore di "<<lancio<<endl;
             //cin nel file
             //t.move(pt,lancio);
             //Cell currCell = get_pos();
                 //if(passaggio per il via)
-                    cout<<"Giocatore "<<playerID<<" è passato dal via e ha ritirato 20 fiorini"<<endl;
-            cout<<"Giocatore "<<playerID<<" è arrivato alla casella "<<currCell; //cosa stampa il << di cell?
+                    ofs<<"Giocatore "<<playerID<<" è passato dal via e ha ritirato 20 fiorini"<<endl;
+            //ofs<<"Giocatore "<<playerID<<" è arrivato alla casella "<<currCell; //cosa stampa il << di cell?
                 //if(casella angolare)
                 //else if (Casella laterale non appartenente nessuno dei giocatori)
                     //if(ha soldi && compra)
-                        //cout<<Giocatore N ha acquistato il terreno Y
+                        //ofs<<Giocatore N ha acquistato il terreno Y
                 //else if (Casella laterale di proprietà giocatore, senza casa/albergo)
                     //if(ha soldi && costruisce casa)
-                        cout<<"Giocatore "<<playerID<<" ha costruito una casa sul terreno"<<currCell<<endl; //stampa info terreno?
+                        //ofs<<"Giocatore "<<playerID<<" ha costruito una casa sul terreno"<<currCell<<endl; //stampa info terreno?
                 //else if (Casella laterale con casa proprietà)
                     //if(ha soldi && migliora in albergo)
-                        cout<<"Giocatore "<<playerID<<"  ha migliorato una casa in albergo sul terreno"<<currCell<<endl; //stampa info terreno?
+                        //ofs<<"Giocatore "<<playerID<<"  ha migliorato una casa in albergo sul terreno"<<currCell<<endl; //stampa info terreno?
                 //else (Casella laterale con casa proprietà)
                     //int valore = currCell.get_value();
                     //if(pt.saldo()>=valore)
                         //currCell.getProprietario().versa(pt.preleva(valore));
-                        //jcout<<<<"Giocatore "<<playerID<<" ha pagato "<<valore<<" fiorini a giocatore "<<currCell.getProprietario().getID()<<" per pernottamento nella casella "<<currCell<<endl; //info su cell
+                        //ofs<<<<"Giocatore "<<playerID<<" ha pagato "<<valore<<" fiorini a giocatore "<<currCell.getProprietario().getID()<<" per pernottamento nella casella "<<currCell<<endl; //info su cell
                         pList.push(pt);
                     //else //non ha abbastanza soldi
-                        cout<<<<"Giocatore "<<playerID<<"è stato eliminato"<<endl;
+                        ofs<<"Giocatore "<<playerID<<"è stato eliminato"<<endl;
                         //NON ESEGUO IL PUSH, eliminato
                         //tutte le sue proprietà vengono rese libere
         }
         Player& winner = pList.front();
-        //cout<<Giocatore N ha vinto la partita
+        //ofs<<Giocatore N ha vinto la partita
+
+        ofs.close();
     } else if (modalitaGioco == "human") {
         //giocaUmano();
         t.stampa(); //ogni volta che viene richiesto
