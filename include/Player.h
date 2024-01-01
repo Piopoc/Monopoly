123cb9f0cfecdb7a8f79f0d46aea829b6ec74b39
levalby <levalby@gmail.com>
2024-01-01 19:35:11 +0100

    meglio

--- include/Player.h
+++ include/Player.h
@@ -4,45 +4,26 @@
 #define PLAYER_H
 #include <vector>
 
-class Cell;
-
 class Player{
     public:
-    //costruttore con id player
-        explicit Player(int ID = 0);
-    //assegnamento per copia
-        Player& operator=(Player& p);
-    //controllo se ha abbastanza denaro da spendere
-        bool has_this_money(int valore);
-    //ottiene il saldo giocatore
-        int get_money();
-    //versa una somma di denaro al giocatore
-        void deposit(int x);
-    //preleva una somma di denaro al giocatore, se non c'è disponibilità lancia eccezione, si presuppone che nel main venga controllato prima se c'è disponibilità per non gestire in try catch
-        void withdraw(int x);
-    //ottieni id del giocatore
-        int get_ID() const;
-    //controlla se puo e sceglie se comprare p=25% computer
-        bool pc_buys(int valore);
-    //dove si trova
-        int get_currpos() const;
-    //dove va
-        void set_currpos(int pos);
+        explicit Player(int ID = 0); //costruttore con id player
+        Player& operator=(Player& p); //assegnamento per copia
+        bool has_this_money(int valore); //controllo se ha abbastanza denaro da spendere
+        int get_money(); //ottiene il saldo giocatore
+        void deposit(int x); //versa una somma di denaro al giocatore
+        void withdraw(int x); //preleva una somma di denaro al giocatore, se non c'è disponibilità lancia eccezione, si presuppone che nel main venga controllato prima se c'è disponibilità per non gestire in try catch
+        int get_ID() const; //ottieni id del giocatore  
+        bool pc_buys(int valore); //controlla se puo e sceglie se comprare p=25% computer
+        int get_currpos() const; //dove si trova nel vector
+        void set_currpos(int pos); //dove va
     private:
-    //fiorini
-        int cash;
-    //id del giocatore
-        int playerID;
-    //cella in cui si trova
-        int currentPos;
+        int cash; //fiorini
+        int playerID; //id del giocatore
+        int currentPos; //cella in cui si trova
 };
-//tira 2 dadi
-int dice();
-//controlla se il lancio più alto ha un pareggio
-bool repeated_max(std::vector<int> a);
-//ottiene posizione del giocatore con punteggio massimo all'interno dell'array
-int get_posmax(std::vector<int> a);
-//rilancia i dadi massimi in pareggio
-void throw_again(std::vector<int> a);
+int dice(); //tira 2 dadi
+bool repeated_max(std::vector<int> a); //controlla se il lancio più alto ha un pareggio
+int get_posmax(std::vector<int> a); //ottiene posizione del giocatore con punteggio massimo all'interno dell'array
+void throw_again(std::vector<int> a); //rilancia i dadi massimi in pareggio
 
 #endif
