80cff037ac7dfaa2e4b0a593f80bc3609c37235f
Filippo Corradi <filippo.corradi@studenti.unipd.it>
2024-01-02 13:04:53 +0100

    merge col mago ok

--- include/tabellone.h
+++ include/tabellone.h
@@ -12,7 +12,6 @@ class tabellone{
         void print_matrix();
         std::shared_ptr<Cell> get_cell(int pos); //da definire che torna la ref alla cell dell'indice array
         void start_game(Player* p1, Player* p2, Player* p3, Player* p4);
-        Cell* get_cell(int pos); //da definire che torna la ref alla cell dell'indice array
         bool beyond_start(Player* p, int from); //da definire
         void move(Player* p, int spostamenti); //da definire
         std::string get_cellname(int pos); //ritorna ad es A1
