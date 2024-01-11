596709bd692965a17be121394449065881c1234e
Filippo Corradi <filippo.corradi@studenti.unipd.it>
2024-01-11 14:34:15 +0100

    final

---
+++ build/CMakeFiles/exe.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exe.dir/src/Cell.cpp.o"
+  "CMakeFiles/exe.dir/src/Cell.cpp.o.d"
+  "CMakeFiles/exe.dir/src/Player.cpp.o"
+  "CMakeFiles/exe.dir/src/Player.cpp.o.d"
+  "CMakeFiles/exe.dir/src/Table.cpp.o"
+  "CMakeFiles/exe.dir/src/Table.cpp.o.d"
+  "CMakeFiles/exe.dir/src/Util.cpp.o"
+  "CMakeFiles/exe.dir/src/Util.cpp.o.d"
+  "CMakeFiles/exe.dir/src/main.cpp.o"
+  "CMakeFiles/exe.dir/src/main.cpp.o.d"
+  "exe"
+  "exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
